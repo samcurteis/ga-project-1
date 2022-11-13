c66ad701df687721bf4a154a2524799ec85a9fb7
samcurteis <samcurteis@gmail.com>
2022-11-13 21:46:59 +0000

    created row 4, started editing moveObject

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,23 @@ function init() {
   const width = 9;
   const gridCellCount = width * width;
   const cells = [];
+  const rowFour = [];
+
   let bootPosition = 76;
   let frisbeePositions = [width * 4, width * 4 + 3, width * 4 + 6];
-  console.log(frisbeePositions);
+  // console.log(frisbeePositions);
 
   function createGrid(startingPosition) {
     for (let index = 0; index < gridCellCount; index++) {
       // console.log("it works!");
       const cell = document.createElement("div");
-      cell.setAttribute("data-index", index);
+      if (index < width * 4 && index > width * 3 - 1) {
+        cell.dataset.row = 4;
+        cell.dataset.index = index;
+        rowFour.push(cell);
+      } else {
+        cell.dataset.index = index;
+      }
       cell.innerHTML = index;
       cells.push(cell);
       grid.appendChild(cell);
@@ -20,37 +28,30 @@ function init() {
     cells[startingPosition].classList.add("boot");
   }
 
-  function addObject(position, className) {
-    cells[position].classList.add(className);
+  console.log(cells, rowFour);
+
+  function addObject(array, position, className) {
+    array[position].classList.add(className);
   }
 
   function removeObject(position, className) {
-    cells[position].classList.remove(className);
+    array[position].classList.remove(className);
   }
 
-  function moveObstacles(className, positionsToHandle, speed) {
+  function moveObstacles(row, className, speed) {
     setInterval(() => {
-      positionsToHandle.forEach((position) =>
-        removeObject(position, className)
-      );
-      // console.log(positionsToHandle);
-      positionsToHandle = positionsToHandle.map((position) => {
-        // ++position);
-        const x = position % width;
-        console.log(position, x);
-        if (x < width - 1) {
-          position = position + 1;
-        } else if (x === width - 1) {
-          position -= width - 1;
-        }
-      });
-      // console.log(positionsToHandle);
-      positionsToHandle.forEach((position) => addObject(position, className));
+      for (let index = 0; index < row.length; index = index = index + 3) {
+        const element = document.createElement("div");
+        row.createElement("div");
+        element.classList.remove(className);
+
+        element.classList.add(className);
+      }
     }, speed);
   }
 
   createGrid(bootPosition);
-  moveObstacles("frisbee", frisbeePositions, 1000);
+  moveObstacles(rowFour, "frisbee", 1000);
 
   function moveBoot(event) {
     removeObject(bootPosition, "boot");
@@ -80,10 +81,3 @@ function init() {
 }
 
 window.addEventListener("DOMContentLoaded", init);
-
-// const x = frisbeePosition.map((frisbee) => frisbee % width);
-// if (x.forEach((i) => i < width - 1) {
-//   frisbeePosition++;
-// } else if (x.forEach((i) => i === width - 1) {
-//   frisbeePosition -= width - 1;
-// }
