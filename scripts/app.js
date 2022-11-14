244af43052f96374328b664bc4d787dd4111ed4c
samcurteis <samcurteis@gmail.com>
2022-11-14 11:58:40 +0000

    lots of changes

--- scripts/app.js
+++ scripts/app.js
@@ -4,49 +4,92 @@ function init() {
   const width = 9;
   const gridCellCount = width * width;
   const cells = [];
-  const rowFour = [];
+  const rowFour = [27, 30, 33];
+  // const rowFour = [];
 
   let bootPosition = 76;
-  let frisbeePositions = [width * 4, width * 4 + 3, width * 4 + 6];
-  // console.log(frisbeePositions);
 
-  function createGrid(startingPosition) {
-    for (let index = 0; index < gridCellCount; index++) {
-      // console.log("it works!");
+  // function createGrid(startingPosition) {
+  //   for (let index = 0; index < gridCellCount; index++) {
+  //     // console.log("it works!");
+  //     const cell = document.createElement("div");
+  //     if (index < width * 4 && index > width * 3 - 1) {
+  //       cell.dataset.row = 4;
+  //       cell.dataset.index = index;
+  //       rowFour.push(cell);
+  //     } else {
+  //       cell.dataset.index = index;
+  //     }
+  //     cell.innerHTML = index;
+  //     cells.push(cell);
+  //     grid.appendChild(cell);
+  //   }
+  //   cells[startingPosition].classList.add("boot");
+  // }
+
+  function createGrid() {
+    for (let i = 0; i < gridCellCount; i++) {
       const cell = document.createElement("div");
-      if (index < width * 4 && index > width * 3 - 1) {
-        cell.dataset.row = 4;
-        cell.dataset.index = index;
-        rowFour.push(cell);
-      } else {
-        cell.dataset.index = index;
-      }
-      cell.innerHTML = index;
+      cell.setAttribute("data-index", i);
       cells.push(cell);
       grid.appendChild(cell);
     }
-    cells[startingPosition].classList.add("boot");
   }
 
   console.log(cells, rowFour);
 
-  function addObject(array, position, className) {
-    array[position].classList.add(className);
+  // function addObject(index, className) {
+  //   index.classList.add(className);
+  // }
+
+  // function removeObject(index, className) {
+  //   index.classList.remove(className);
+  // }
+
+  // function moveObstacles(row, className, speed) {
+  //   setInterval(
+  //     () => {
+  //       rowFour.forEach((index) => removeObject(index, "frisbee"))
+  //       for (let index = 0; index < row.length; index = index + 3) {
+  //         row[index - 1].classList.remove(className);
+  //         const element = row[index];
+  //         // const newObject = document.createElement("div");
+  //         element.classList.add(className);
+
+  //         const newDiv = document.createElement("div");
+  //         row.push(newDiv);
+  //       }
+  //       rowFour.forEach((index) => addObject(index, "frisbee"));
+  //     },
+
+  //     speed
+  //   );
+  // }
+  // moveObstacles(rowFour, "frisbee", 1000);
+
+  function addObject(row, className) {
+    row.forEach((index) => {
+      cells[index].classList.add(className);
+    });
   }
 
-  function removeObject(position, className) {
-    array[position].classList.remove(className);
+  function removeObject(row, className) {
+    row.forEach((index) => {
+      cells[index].classList.remove(className);
+    });
   }
 
   function moveObstacles(row, className, speed) {
     setInterval(() => {
-      for (let index = 0; index < row.length; index = index = index + 3) {
-        const element = document.createElement("div");
-        row.createElement("div");
-        element.classList.remove(className);
-
-        element.classList.add(className);
-      }
+      removeObject(row, className);
+      row = row.map((index) => {
+        if (index > width * 4 - 2) {
+          return (index -= width + -1);
+        } else {
+          return (index += 1);
+        }
+      });
+      addObject(row, className);
     }, speed);
   }
 
